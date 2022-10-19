0bcdce212051e613ad56dffecaf1391eff9600fe
Jonathan Ramos Pereira <desenvjonathan@hotmail.com>
2022-10-19 19:56:01 -0300

    inicial commit

---
+++ comentarios.js
@@ -0,0 +1,12 @@
+// Aqui eu tenho um comentário em linha
+
+/*
+Aqui eu tenho um comentário multi linha
+*/
+
+/* o código abaixo escreve alguma mensagem no console, 
+nesse caso "Bem vindos ao Starter", no DevTools
+ console.log('Bem vindos ao Starter') */
+
+/* no código fonte os comentários são mostrados, mas na execução do console log 
+e no HTML, a página está vazia */
---
+++ sintaxe.js
@@ -0,0 +1,9 @@
+// A importância da Sintaxe
+
+//  * Toda linguagem tem
+//  * Uma boa comunicação necessita de uma boa sintaxe
+//  * 82% dos erros para iniciantes da programação
+//  * Sintaxe de forma incorreta pode estragar seu código, seja um ponto
+//seja um ponto em um lugar incorreto, ou um espaço onde não tem, uma vírgula ou aspas
+
+console.log("Bem vindos ao Starter") // forma correta de se escrever
